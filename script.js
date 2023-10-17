aa483db7eb91b6bbf49bbad3f51d128a4dffe250
thishastobenoah <thishastobenoah@gmail.com>
2023-10-16 21:55:42 -0400

    submission

---
+++ script.js
@@ -0,0 +1,25 @@
+const firstName = "Noah";
+const lastName = "Cranson";
+const fullName = firstName + " " + lastName;
+
+console.log(fullName);
+
+let population = 639111;
+console.log(population);
+
+population *= 3;
+console.log(population);
+
+let javaScriptIsCool = true;
+
+let colors = ["yellow", "green", "pink"];
+
+for (i=1; i<6; i++){
+    console.log(i);
+}
+
+for (i=0; i<5; i++){
+    console.log(i);
+}
+
+
